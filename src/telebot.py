101adcf397993efb52b0a5c5a4b75c30e009c228
biseshbhattarai <aashishbhattarai49@gmail.com>
2018-10-15 13:44:43 +0545

    Weather and news feature added

--- src/telebot.py
+++ src/telebot.py
@@ -3,8 +3,38 @@ import requests
 import time
 import config
 
-TOKEN = config.TOKEN(# Add your token)
+
+
+TOKEN = config.TOKEN
 URL = "https://api.telegram.org/bot{}/".format(TOKEN)
+API_KEY = config.API_KEY
+WEATHER_URL = "https://api.apixu.com/v1/current.json?key={}&q=Paris".format(API_KEY)
+HACKER_NEWS_API_KEY = config.HACKER_NEWS_API_KEY
+HACKER_NEWS_BASE_URL = 'https://newsapi.org/v2/top-headlines?country=us&apiKey='+HACKER_NEWS_API_KEY
+
+
+messages = ['Weather info', 'weather', 'weather info']
+
+
+def get_weather_info(city):
+    response = requests.get("https://api.apixu.com/v1/current.json?key={}&q={}".format(API_KEY, city))
+    content = response.content.decode("utf8")
+    js = json.loads(content)
+
+    data = "Current temperature : {} , Current pressure :  {} , Current wind speed : {},  wind direction : {} , humidity : {}".format(js['current']['temp_c'], js['current']['pressure_mb'], js['current']['wind_mph'], js['current']['wind_dir'], js['current']['humidity'])
+    return  data
+
+def get_latest_news_from_hacker_news(i):
+    response = requests.get(HACKER_NEWS_BASE_URL)
+    content = response.content.decode("utf8")
+    js = json.loads(content)
+    i = i
+
+    return ( js['articles'][i]['source']['name'], js['articles'][i]['title'], js['articles'][i]['description'],js['articles'][i]['url'], js['articles'][i]['content'])
+  
+
+
+
 
 def get_url(url):
     response = requests.get(url)
@@ -25,32 +55,44 @@ def get_updates(offset=None):
     js = get_json_from_url(url)
     return js
 
+
+def send_message(text, chat_id):
+    url = URL + "sendMessage?text={}&chat_id={}".format(text, chat_id)
+    get_url(url)
+
+
 def get_last_update_id(updates):
     update_ids = []
     for update in updates["result"]:
         update_ids.append(int(update["update_id"]))
     return max(update_ids)
 
-def echo_all(updates):
-    for update in updates["result"]:
-        try:
-            text = update["message"]["text"]
-            chat = update["message"]["chat"]["id"]
-            send_message(text, chat)
-        except Exception as e:
-            print(e)
 
 def get_last_chat_id_and_text(updates):
     num_updates = len(updates["result"])
     last_update = num_updates - 1
     text = updates["result"][last_update]["message"]["text"]
     chat_id = updates["result"][last_update]["message"]["chat"]["id"]
-    return (text, chat_id)
+    send_message("Here is the weather info of {}".format(text), chat_id)
 
 
-def send_message(text, chat_id):
-    url = URL + "sendMessage?text={}&chat_id={}".format(text, chat_id)
-    get_url(url)
+
+def echo_all(updates):
+    for update in updates["result"]:
+        try:
+            
+            text = update["message"]["text"]
+            chat = update["message"]["chat"]["id"]
+            if text == 'tech news':
+                for i in range(10):
+                    send_message(get_latest_news_from_hacker_news(i), chat)
+            else:
+                send_message("Here is the weather info of {}".format(text), chat)
+                send_message(get_weather_info(text), chat)
+
+             
+        except Exception as e:
+            print(e)
     
 def main():
     last_update_id = None
